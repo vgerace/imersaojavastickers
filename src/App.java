66989f9f0e3835c0fc1d9eb8f0808eaa0d43b6b3
vitor.borges <vitor.borges@accesstage.com.br>
2023-03-30 23:02:46 -0300

    Finalizacao da aula 2 e seus desafios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -8,11 +12,12 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         // Realizar uma conexão HTTP e buscar os top 250 filmes
         // Buscar na variável de ambiente a API Key
         String imdbKey = System.getenv("IMDB_API_KEY");
-        String url = "https://imdb-api.com/en/API/Top250Movies/" + imdbKey;
+        // String url = "https://imdb-api.com/en/API/Top250Movies/" + imdbKey;
+        String url = "https://imdb-api.com/en/API/MostPopularMovies/" + imdbKey;
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -22,22 +27,48 @@ public class App {
         // Extrair só os dados que interessam (título, imagem, classificação)
 
         var parser = new JsonParser();
-
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 //  O size retorna o tamanho da lista
 //  System.out.println(listaDeFilmes.size());
 //  O get(0) retorna o primeiro elemento do índice
 //  System.out.println(listaDeFilmes.get(0));
 
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
 
-        // Exibir a manipulação dos dados
+        var geradora = new GeradoraDeFigurinhas();
+        for (int index = 0; index < 5; index++) {
+            var filme = listaDeFilmes.get(index);
 
-//        for (Map<String, String> filme : listaDeFilmes) {
-        for (int i = 0; i < 10; i++) {
-            Map<String, String> filme = listaDeFilmes.get(i);
-            System.out.println("\uD83C\uDFAC\u001b[1m\u001b[37m\u001b[43mTitle:\u001b[m " + filme.get("title"));
-            System.out.println("\uD83C\uDF9E\u001b[1m\u001b[44mImage URL:\u001b[m\u001b[m" + " " + filme.get("image"));
+
+            // Exibir a manipulação dos dados
+            // Exibir apenas 10 - for (int i = 0; i < 10; i++) {Map<String, String> filme = listaDeFilmes.get(i);}
+            // for (Map<String, String> filme : listaDeFilmes) {
+            String urlImagem = filme.get("image");
+            String urlImagemMaior = urlImagem.replaceFirst("(@?\\.)([0-9A-Z,_]+).jpg$", "$1.jpg");
+            String titulo = filme.get("title");
             double classificacao = Double.parseDouble(filme.get("imDbRating"));
+
+            String textoFigurinha;
+            InputStream imagemVitor;
+
+            if (classificacao >= 8.0) {
+                textoFigurinha = "TOPD+";
+                imagemVitor = new FileInputStream(new File("/home/vgerace/Workspaces/Alura/imersaojava2023/alura-stickers/sobreposicao/VitorTop.jpeg"));
+            } else {
+                textoFigurinha = "HMMMMM...";
+                imagemVitor = new FileInputStream(new File("/home/vgerace/Workspaces/Alura/imersaojava2023/alura-stickers/sobreposicao/VitorBad.jpeg"));
+            }
+
+
+            InputStream inputStream = new URL(urlImagemMaior).openStream();
+            String nomeArquivo = "figurinhas/" + titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo, textoFigurinha, imagemVitor);
+
+            System.out.println("\uD83C\uDFAC\u001b[1m \u001b[30m\u001b[43mTitle:\u001b[m" + " " + titulo);
+            System.out.println("\uD83C\uDF9E\u001b[1m \u001b[44mImage URL:\u001b[m\u001b[m" + " " + urlImagem);
+            System.out.println("\uD83D\uDCCF\u001b[1m \u001b[45mClassificação:\u001b[m\u001b[m" + " " + classificacao);
             int numeroEstrelinhas = (int) classificacao;
             for (int n = 1; n <= numeroEstrelinhas; n++) {
                 if (numeroEstrelinhas > 9)
